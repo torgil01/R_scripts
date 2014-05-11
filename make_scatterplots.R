2e6172edc845f5fcaa8c740b17b5d390af96ee72
torgil01 <torgil.vangberg@gmail.com>
2014-05-11 02:22:12 +0200

    added commands to save plots

--- make_scatterplots.R
+++ make_scatterplots.R
@@ -1,7 +1,10 @@
 # script for making scatterplots
 
-#mkScatterPlot(AD,"PH_ACC","FA_ALL_PHACC","Phonemic accuracy (Z-score)", "Mean FA", "Phonemic Accuracy vs. FA")
-#AD <- transform(AD, MD_ALL_PHACC = MD_ALL_PHACC*1000)
+dev.copy(file="FA_PH_ACC.pdf",device=pdf, bg="white") 
+graphics.off()
+
+mkScatterPlot(AD,"PH_ACC","FA_ALL_PHACC","Phonemic accuracy (Z-score)", "Mean FA", "Phonemic Accuracy vs. FA")
+ggsave("FA_PH_ACC.pdf")
 Y.lab <- expression(paste('Mean MD [',10^-3,'*',mm^2,'/s]'))
 mkScatterPlot(AD,"PH_ACC","MD_ALL_PHACC","Phonemic accuracy (Z-score)", 
 							Y.lab, "Phonemic Accuracy vs. MD",yscale=1000)
